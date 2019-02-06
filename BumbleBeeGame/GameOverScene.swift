a4a6b8ee8844f782e2c2570c14d08d1e05108806
canadianExperience <lena2157@mail.ru>
2019-02-06 16:02:28 -0500

    score and lives counting set; start and game over screens created

---
+++ BumbleBeeGame/GameOverScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameOverScene.swift
+//  BumbleBeeGame
+//
+//  Created by Elena Melnikova on 2019-02-06.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import Foundation
---
+++ BumbleBeeGame/InstructionsScene.swift
@@ -0,0 +1,9 @@
+//
+//  InstructionsScene.swift
+//  BumbleBeeGame
+//
+//  Created by Elena Melnikova on 2019-02-06.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import Foundation
